813474f529bab8ad499d103aac389cbf19f9a7a0
Evgeniy Ryzhikov <evgeniy.ryzhikov@gmail.com>
2023-05-03 15:19:45 +0700

    Модуль 24. Добавлен RV на главный экран, активити для просмотра деталей о фильме

---
+++ app/src/main/java/evgeniy/ryzhikov/filmsearch/recycler_view/Film.kt
@@ -0,0 +1,11 @@
+package evgeniy.ryzhikov.filmsearch.recycler_view
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Film(
+    val title: String,
+    val poster: Int,
+    val description: String
+) : Parcelable
