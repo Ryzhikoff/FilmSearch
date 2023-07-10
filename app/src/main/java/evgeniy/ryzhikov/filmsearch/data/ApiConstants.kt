86c7dbe6161ad4f8ee56305f5a8481d0c0a68bed
Evgeniy Ryzhikov <evgeniy.ryzhikov@gmail.com>
2023-07-10 16:17:19 +0700

    Модуль 35 Получение фильмов через API Tmdb

---
+++ app/src/main/java/evgeniy/ryzhikov/filmsearch/data/ApiConstants.kt
@@ -0,0 +1,6 @@
+package evgeniy.ryzhikov.filmsearch.data
+
+object ApiConstants {
+    const val BASE_URL = "https://api.themoviedb.org/"
+    const val IMAGE_URL = "https://image.tmdb.org/t/p/"
+}
--- app/src/main/java/evgeniy/ryzhikov/filmsearch/view/rv_viewholders/FilmViewHolder.kt
+++ app/src/main/java/evgeniy/ryzhikov/filmsearch/view/rv_viewholders/FilmViewHolder.kt
@@ -3,6 +3,7 @@ package evgeniy.ryzhikov.filmsearch.view.rv_viewholders
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import evgeniy.ryzhikov.filmsearch.data.ApiConstants
 import evgeniy.ryzhikov.filmsearch.databinding.FilmItemBinding
 import evgeniy.ryzhikov.filmsearch.domain.Film
 
@@ -16,7 +17,7 @@ class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //Указываем контейнер, в котором будет "жить" наша картинка
         Glide.with(itemView)
             //Загружаем сам ресурс
-            .load(film.poster)
+            .load(ApiConstants.IMAGE_URL + "w342" + film.poster)
             //Центруем изображение
             .centerCrop()
             //Указываем ImageView, куда будем загружать изображение
