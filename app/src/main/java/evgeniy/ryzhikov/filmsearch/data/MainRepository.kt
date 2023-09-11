949d828c5472909a28966f3fa06e996c5fad269c
Evgeniy Ryzhikov <evgeniy.ryzhikov@gmail.com>
2023-09-11 12:14:27 +0700

    - Модуль 43 - сипользование Coroutines Flow при получение данных из БД и сети

--- app/src/main/java/evgeniy/ryzhikov/filmsearch/data/MainRepository.kt
+++ app/src/main/java/evgeniy/ryzhikov/filmsearch/data/MainRepository.kt
@@ -1,19 +1,15 @@
 package evgeniy.ryzhikov.filmsearch.data
 
-import androidx.lifecycle.LiveData
 import evgeniy.ryzhikov.filmsearch.data.dao.FilmDao
 import evgeniy.ryzhikov.filmsearch.data.entity.Film
-import java.util.concurrent.Executors
+import kotlinx.coroutines.flow.Flow
 
 class MainRepository(private val filmDao: FilmDao) {
     fun putToDB(films: List<Film>) {
-        //запросы в БД в отдельном потоке
-        Executors.newSingleThreadExecutor().execute {
-            filmDao.insertAll(films)
-        }
+        filmDao.insertAll(films)
     }
 
-    fun getAllFromDB(): LiveData<List<Film>> {
+    fun getAllFromDB(): Flow<List<Film>> {
         return filmDao.getCashedFilms()
     }
 }
--- app/src/main/java/evgeniy/ryzhikov/filmsearch/data/dao/FilmDao.kt
+++ app/src/main/java/evgeniy/ryzhikov/filmsearch/data/dao/FilmDao.kt
@@ -1,17 +1,17 @@
 package evgeniy.ryzhikov.filmsearch.data.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import evgeniy.ryzhikov.filmsearch.data.entity.Film
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FilmDao {
     //запрос на всю таблицу
     @Query("SELECT * FROM cashed_films")
-    fun getCashedFilms(): LiveData<List<Film>>
+    fun getCashedFilms(): Flow<List<Film>>
 
     //Кладем спископ в БД, в случае конфликта - перезаписываем
     @Insert(onConflict = OnConflictStrategy.REPLACE)
